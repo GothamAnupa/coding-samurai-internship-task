32f04acbed05e4f72ad127033c072f0c96a77235
GothamAnupa <gothamanupa@gmail.com>
2025-07-03 11:47:05 +0530

    Update stock_predictor.py

--- StockPricePrediction/stock_predictor.py
+++ StockPricePrediction/stock_predictor.py
@@ -1,11 +1,12 @@
+# 📈 Stock Price Prediction using Linear Regression
+
+# Step 1: Import libraries
+import yfinance as yf
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
-import os
-import yfinance as yf # Import the yfinance library
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import numpy as np
 
 # --- Configuration ---
 # Define the name of your stock data CSV file.
